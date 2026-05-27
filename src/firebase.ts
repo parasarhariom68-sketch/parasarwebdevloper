a82911edbb1ccb2538a24c4551fe119021a8182f
parasarhariom68-sketch <parasarhariom68@gmail.com>
2026-05-28 01:47:21 +0530

    Add Firestore database

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBg9KX8aSg8NtlcSPp_IyBP9osWzq4RslQ",
@@ -12,4 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
